b6a03368aea98468931db7e8d2b39327e8916767
hr951 <hiroki.saki.951@gmail.com>
2026-04-01 13:04:19 +0900

    チャットコマンドの誘導を追加

---
+++ events/messageCreate.js
@@ -0,0 +1,15 @@
+const { basic_embed } = require("../utils/embeds.js");
+
+module.exports = {
+    name: 'messageCreate',
+    async execute(message, client) {
+        if (message.author.bot) return;
+
+        if (message.content.startsWith("!")) {
+            if (message.content.startsWith("!invite") || message.content.startsWith("!add") || message.content.startsWith("!remove") || message.content.startsWith("!list")) {
+                const embed = basic_embed("使用できるコマンドは以下の通りです", ' - `/add`...追跡するユーザーを追加します\n - `/remove`...追跡するユーザーを削除します\n - `/list`...追跡しているユーザーを表示します', '#3498db');
+                message.reply({ embeds: [embed] });
+            }
+        }
+    },
+};
